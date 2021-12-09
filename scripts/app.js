773e7bfd8bc9be30de6a68c5d245456f5a80102d
Morph78 <mahdi.hajilo123@gmail.com>
2021-12-09 18:31:00 +0330

    Upload project on Github

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+let menu = document.querySelector(".menu")
+let menuBtn = document.querySelector(".header__icon")
+let menuBtnIcon = document.querySelector(".header__icon i")
+
+menuBtn.addEventListener("click", function () {
+    if(menuBtnIcon.classList.contains("fa-bars")){
+        menu.style.left = "0"
+        menuBtnIcon.classList ="fas fa-times"
+    }
+    else {
+        menu.style.left = "-256px"
+        menuBtnIcon.classList = "fas fa-bars"
+    }
+})
